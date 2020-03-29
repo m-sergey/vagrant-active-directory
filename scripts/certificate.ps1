4357c732345acf1a6c7f44f0249ba908e3e93184
Andreas Gohr <andi@splitbrain.org>
2020-03-29 19:25:19 +0200

    added localhost and aliases to certificate
    Seems not to make a difference to make verify_peer_name happy in PHP
    though.

--- scripts/certificate.ps1
+++ scripts/certificate.ps1
@@ -10,7 +10,7 @@ param
 Write-Output "Creating certifcate..."
 
 $params = @{
-    DnsName = "$hostName.$domainName"
+    DnsName = "$hostName.$domainName", "127.0.0.1", "localhost", "localhost.localdomain", "::1"
     KeyLength = 2048
     KeyAlgorithm = 'RSA'
     HashAlgorithm = 'SHA256'
@@ -23,4 +23,4 @@ $dcCert = New-SelfSignedCertificate @params
 # Extra step needed since self-signed cannot be directly shipped to trusted root CA store
 # if you want to silence the cert warnings on other systems you'll need to import the cert.der on them too
 Export-Certificate -Cert $dcCert -FilePath "C:\vagrant\cert.der"
-Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\vagrant\rootCA.der"
+Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\vagrant\cert.der"
