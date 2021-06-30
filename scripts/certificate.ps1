a5c1de95b8cf786a2d941112d3401f13ea4de3cd
Andreas Gohr <andi@splitbrain.org>
2021-06-30 08:37:18 +0200

    make path in cert export configurable

--- scripts/certificate.ps1
+++ scripts/certificate.ps1
@@ -5,6 +5,7 @@ param
 (
     [string]$hostName = "dc",
     [string]$domainName = "example.local"
+    [string]$localPath = "C:\vagrant"
 )
 
 Write-Output "Creating certifcate..."
@@ -22,5 +23,5 @@ $dcCert = New-SelfSignedCertificate @params
 
 # Extra step needed since self-signed cannot be directly shipped to trusted root CA store
 # if you want to silence the cert warnings on other systems you'll need to import the cert.der on them too
-Export-Certificate -Cert $dcCert -FilePath "C:\vagrant\cert.der"
-Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\vagrant\cert.der"
+Export-Certificate -Cert $dcCert -FilePath "$localPath\cert.der"
+Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "$localPath\cert.der"
